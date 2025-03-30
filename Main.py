2878fa19c6384906690d51a9a2bb964d26c46c6a
jaboi313 <hollanderkelvin@gmail.com>
2025-03-30 22:31:09 +0200

    Added name=main check

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ from ClassAnagram import Anagram
 csv = CSV()
 anagram = Anagram("Lengte mijnen", csv.read_whole_column('Station'))
 
-print(anagram.anagram())
-print(anagram.get_combinations_made())
-print(csv.get_row_from_value(anagram.anagram(), 'Station'))
-print(csv.get_row_from_value_one_colum(anagram.anagram(), 'Station', 'Code'))
+if __name__ == "__main__":
+    print(anagram.anagram())
+    print(anagram.get_combinations_made())
+    print(csv.get_row_from_value(anagram.anagram(), 'Station'))
+    print(csv.get_row_from_value_one_colum(anagram.anagram(), 'Station', 'Code'))
